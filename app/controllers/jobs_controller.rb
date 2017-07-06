e9e27ef9afb6a39e65c8759103167c3e25e3bd96
House_Leung <House.liangshihao@gmail.com>
2017-07-06 19:07:34 +0800

    implement job is_hidden button

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -2,11 +2,11 @@ class JobsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
 
   def index
-    @jobs = Job.all
+    @jobs = Job.where(:is_hidden => false).order("created_at DESC")
   end
 
   def show
-    @job = Job.fiond(params[:id])
+    @job = Job.find(params[:id])
   end
 
   def new
@@ -44,6 +44,6 @@ class JobsController < ApplicationController
   private
 
   def job_params
-    params.require(:job).permit(:title, :description, :wage_lower_bound, :wage_upper_bound, :contact_email)
+    params.require(:job).permit(:title, :description, :wage_lower_bound, :wage_upper_bound, :contact_email, :is_hidden)
   end
 end
